c9a10cc1d146fe4e5ecb3043177b389b27861ed6
padmapriyar04 <ae22b040@smail.iitm.ac.in>
2023-11-02 21:43:13 +0530

    User.ts added

--- src/mongoose_connect.ts
+++ src/mongoose_connect.ts
@@ -1,20 +1,8 @@
-// db.ts
 import mongoose from 'mongoose';
 
-const connection = {};
-
 async function connectDB() {
-  if (connection.isConnected) {
-    return;
-  }
-
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  });
-
-  connection.isConnected = db.connections[0].readyState;
+  const db = await mongoose.connect(process.env.MONGODB_URI ?? "mongodb://localhost:27017");
+  console.log("Connection successful")
 }
 
 export default connectDB;
